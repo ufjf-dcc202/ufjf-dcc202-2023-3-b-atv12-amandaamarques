a2a996754de6bebe8518a26d2144094d3b9f7cab
amandaamarques <131484602+amandaamarques@users.noreply.github.com>
2023-12-11 12:44:45 -0300

    'buttonLimparLista': limpa estoque+atualiza a tela

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
 
 document.entrada.addEventListener('submit', leFormulario);
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
 
 atualizaTela();
 function leFormulario(event) {
